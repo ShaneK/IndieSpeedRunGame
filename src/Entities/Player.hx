0b474fef2b605d1c858b36268ba81d9949c040de
Ben Van Treese <vantreeseba@gmail.com>
2013-09-14 00:02:50 -0500

    Added player sprite and started on sprite controller

--- src/Entities/Player.hx
+++ src/Entities/Player.hx
@@ -3,9 +3,10 @@ package entities;
 import com.haxepunk.Entity;
 import com.haxepunk.HXP;
 import com.haxepunk.graphics.Image;
+import com.haxepunk.graphics.Spritemap;
 import com.haxepunk.utils.Input;
 import com.haxepunk.utils.Key;
- 
+
 import nape.phys.Body;
 import nape.phys.BodyType;
 import nape.shape.Polygon;
@@ -19,6 +20,7 @@ class Player extends PhysicalBody
 	private var image:Image;
     private var velocityX:Float = 0;
     private var velocityY:Float = 0;
+    private var sprite:Spritemap;
 
     var maximumSpeed = 250;
     var jumpVelocity = 0;
@@ -35,19 +37,34 @@ class Player extends PhysicalBody
         body.setShapeMaterials(Material.steel());
         body.allowRotation = false;
         body.mass = 10;
+        
+        sprite = new Spritemap("gfx/player.png", 32, 64);        
+        sprite.add("idle", [0]);        
+        sprite.add("walk", [1, 2, 3, 4, 5], 8, true);
+        sprite.add("jump", [19]);
+        sprite.scaledWidth = width;
+        sprite.scaledHeight = height;
+        
+        // // tell the sprite to play the idle animation
+        // sprite.play("idle");
+ 
+        // // apply the sprite to our graphic object so we can see the player
+         graphic = sprite;
 
-        image = Image.createRect(width,height, 0xFF0000);
-        graphic = image;
+        // defines left and right as arrow keys and WASD controls
+        Input.define("left", [Key.LEFT, Key.A]);
+        Input.define("right", [Key.RIGHT, Key.D]);
+        Input.define("jump", [Key.UP, Key.W, Key.SPACE]);
     }
 
     public override function update(){
     	super.update();
     	x = body.position.x;
     	y = body.position.y;
-    	image.angle = body.rotation;
         // followMe();
         handleInput();
         velocityManagement();
+        setAnimations();
     }
 
     public function followMe(){
@@ -57,27 +74,44 @@ class Player extends PhysicalBody
     }
 
     public function handleInput(){
-        if (Input.check(Key.LEFT))
+        if (Input.check("left"))
         {
             velocityX = EMath.clamp(velocityX+(maximumSpeed*.4), -maximumSpeed, maximumSpeed);
-            if(body.velocity.x > 0){
-                velocityX = EMath.clamp(velocityX, -maximumSpeed, 0);
-            }
+            // if(body.velocity.x > 0){
+            //     velocityX = EMath.clamp(velocityX, -maximumSpeed, 0);
+            // }
         }
  
-        if (Input.check(Key.RIGHT))
+        if (Input.check("right"))
         {
             velocityX = EMath.clamp(velocityX-(maximumSpeed*.4), -maximumSpeed, maximumSpeed);
-            if(body.velocity.x < 0){
-                velocityX = EMath.clamp(velocityX, 0, maximumSpeed);
-            }
+            // if(body.velocity.x < 0){
+            //     velocityX = EMath.clamp(velocityX, 0, maximumSpeed);
+            // }
         }
         
-        if (Input.check(Key.SPACE) && body.velocity.y == 0)
+        if (Input.check("jump") && body.velocity.y == 0)
         {
             jumpVelocity = 200;
         }
     }
+
+     private function setAnimations()
+     {
+        if (body.velocity.x > 1 || body.velocity.x < -1)
+        {
+            sprite.play("walk");
+            sprite.flipped = velocityX > 0;            
+        }
+        else
+        {
+            sprite.play("idle");
+        }
+
+        if(body.velocity.y > 1 || body.velocity.y < -1){
+            sprite.play("jump");
+        }
+     }
 
     public function velocityManagement(){
         if(body.velocity.y != 0){
